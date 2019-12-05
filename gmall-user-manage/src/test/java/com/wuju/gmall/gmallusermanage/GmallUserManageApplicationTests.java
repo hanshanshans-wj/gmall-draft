90ce2412addd5b86740693e2f738a92833911e7a
吴飓 <wuju1995123@163.com>
2019-12-05 15:24:46 +0800

    kelong1

---
+++ gmall-user-manage/src/test/java/com/wuju/gmall/gmallusermanage/GmallUserManageApplicationTests.java
@@ -0,0 +1,14 @@
+package com.wuju.gmall.gmallusermanage;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUserManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
